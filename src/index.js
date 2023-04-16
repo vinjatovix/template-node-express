3521dd6570819831e4691ed02abb439d728eedf2
vinjadevix <vinjatovix@gmail.com>
2023-04-16 03:52:07 +0200

    feat(service): initialize api

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 const express = require("express");
+const timeout = require("connect-timeout");
 const cors = require("cors");
 const { apiLimiter, errorHandler, requestMonitor, securityPolicy } = require("./middlewares");
 const logger = require("./service/logger");
+const { v1 } = require("./api/router");
 
 const { NODE_ENV, HOST, PORT, TRUSTED_PROXIES } = process.env;
 
 const app = express();
+app.locals.startTime = Date.now();
 
+const defaultResponse = (_req, res) => {
+  res.status(404).send({ message: "Not Found" });
+};
 app
-  .set("strict routing", true)
   .set("x-powered-by", false)
   .set("trust proxy", TRUSTED_PROXIES)
   .use(cors())
   .use(requestMonitor)
   .use(apiLimiter)
   .use(securityPolicy)
-  .get("/*", async (_req, _res, next) => {
-    try {
-      throw { id: "MODEL_NOT_FOUND", replace: { id: "123", modelName: "User" } };
-    } catch (error) {
-      next(error);
-    }
-  })
+  .use(timeout(30000))
+  .use(v1)
+  .get("/*", defaultResponse)
   .use(errorHandler)
   .listen(PORT, () => {
     logger.info(`${NODE_ENV} server is running on ${HOST}:${PORT}.`);
